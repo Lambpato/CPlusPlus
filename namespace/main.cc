7f1082960681e7d55d05f7140af53f50dfa43913
Lamberto Martinez <lambpato@gmail.com>
2023-11-06 05:23:58 +0000

    went over namespace

---
+++ namespace/main.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+namespace first
+{
+  int x = 1;
+}
+
+namespace second
+{
+  int x = 2;
+}
+
+int main()
+{
+  // Namespace = provides a solution for preventing name conflicts
+  // in large projects. Each entity needs a unique name.
+  // A namespace allows for identically named entities
+  // as long as the namespaces are different.
+
+  int x = 0;
+
+  std::cout << x << std::endl;
+  std::cout << first::x << std::endl;
+  std::cout << second::x << std::endl;
+
+  return 0;
+}
